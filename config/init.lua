4b18e3e9068b520879c851670d8962797b40fb1f
Oliver Marriott <hello@omarriott.com>
2021-09-11 15:00:10 +1000

    use provides-fennel hotpot branch

--- config/init.lua
+++ config/init.lua
@@ -19,7 +19,7 @@ local hotpot_path = vim.fn.stdpath("data") ..
 if vim.fn.empty(vim.fn.glob(hotpot_path)) > 0 then
     print("Could not find hotpot.nvim, cloning new copy to", hotpot_path)
     vim.fn.system({
-        "git", "clone", "https://github.com/rktjmp/hotpot.nvim", hotpot_path
+        "git", "clone", "-b", "provides-fennel", "https://github.com/rktjmp/hotpot.nvim", hotpot_path
     })
 end
 
